0ebd5ae87353dcd86bf261b351b93e24e3011bd0
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2026-06-18 23:42:46 +0100

    Create entity.py

---
+++ custom_components/alfen_modbus/entity.py
@@ -0,0 +1,36 @@
+"""Entities for the Alfen Modbus integration."""
+
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.core import callback
+from homeassistant.helpers.entity import Entity, EntityDescription
+
+from . import AlfenModbusHub
+
+
+class AlfenEntity(Entity):
+    """Representation of an Alfen Modbus entity."""
+
+    # _attr_has_entity_name = True
+    _attr_should_poll = False
+
+    def __init__(
+        self,
+        hub: AlfenModbusHub,
+        device_info: DeviceInfo,
+    ) -> None:
+        """Initialize the Alfen Modbus entity."""
+        self._hub = hub
+        self._attr_device_info = device_info
+
+    async def async_added_to_hass(self) -> None:
+        """Register callbacks."""
+        self._hub.async_add_alfen_sensor(self._modbus_data_updated)
+
+    async def async_will_remove_from_hass(self) -> None:
+        """Remove callbacks."""
+        self._hub.async_remove_alfen_sensor(self._modbus_data_updated)
+
+    @callback
+    def _modbus_data_updated(self) -> None:
+        """Handle updated data from the hub."""
+        self.async_write_ha_state()
